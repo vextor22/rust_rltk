41644e4160cd865403c527345c8d714579366626
Matt Higgins <mrhiggi@g.clemson.edu>
2022-06-01 20:12:26 -0400

    Feature/walking a map (#2)
    * Add rooms
    
    * Add rooms
    
    * Add comment to web page
    
    Co-authored-by: Matthew Higgins <matt@Matthews-MacBook-Pro.local>

---
+++ src/map.rs
@@ -0,0 +1,107 @@
+use super::Rect;
+use rltk::{RandomNumberGenerator, Rltk, RGB};
+use std::cmp::{max, min};
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+/// Makes a map with solid boundaries and randomly placed walls
+/// which may look awful.
+pub fn new_map_test() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    // Make boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+/// Draw a slice of the map
+pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.5, 0.5, 0.5),
+                RGB::from_f32(0., 0., 0.),
+                rltk::to_cp437('.'),
+            ),
+            TileType::Wall => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.0, 1.0, 0.0),
+                RGB::from_f32(0., 0.1, 0.1),
+                rltk::to_cp437('#'),
+            ),
+        }
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
+    let mut map = vec![TileType::Wall; 80 * 50];
+
+    let mut rng = RandomNumberGenerator::new();
+    let starting_room = Rect::new(30, 20, 10, 15);
+    apply_room_to_map(&starting_room, &mut map);
+    for _i in 0..20 {
+        let x = rng.range(1, 65);
+        let y = rng.range(1, 35);
+        let w = rng.range(3, 10);
+        let h = rng.range(3, 10);
+
+        let new_rect = Rect::new(x, y, w, h);
+        apply_room_to_map(&new_rect, &mut map);
+    }
+
+    map
+}
+
+fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
+    for y in room.y1 + 1..=room.y2 {
+        for x in room.x1 + 1..=room.x2 {
+            map[xy_idx(x, y)] = TileType::Floor;
+        }
+    }
+}
+
+//fn apply horizontal_tunnel(map: &mut [TileType], x1:i32, x2:i32, y:i32){
+//    for x in min(x1,x2)..=(max(x1,x2)){
+//        let idx = xy_idx(x,y);
+//        if idx > 0 && idx < 80*50{
+//
+//        }
+//    }
+//}
